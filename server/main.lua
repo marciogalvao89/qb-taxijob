ed77eb3455214b2f6fbb0299e5eca9576b2898db
marciogalvao89 <32371159+marciogalvao89@users.noreply.github.com>
2023-09-27 23:18:33 +0100

    1
    1

--- server/main.lua
+++ server/main.lua
@@ -19,7 +19,7 @@ RegisterNetEvent('qb-taxi:server:NpcPay', function(Payment)
             local randomAmount = math.random(1, 5)
             local r1, r2 = math.random(1, 5), math.random(1, 5)
             if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
-            Player.Functions.AddMoney('cash', Payment)
+            Player.Functions.AddMoney('cash', Payment, "Taxi")
             local chance = math.random(1, 100)
             if chance < 26 then
                 Player.Functions.AddItem("cryptostick", 1, false)
